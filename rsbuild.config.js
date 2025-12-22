8c29dfe0c7320f72d026f3ddf13071d50d131503
leminhhieu98py <leminhhieu98py@gmail.com>
2025-12-23 00:11:18 +0700

    fix: fix PROD build issue

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginLess } from "@rsbuild/plugin-less";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   source: {
     entry: {
       index: "./index.js",
     },
+  },
+  output: {
+    assetPrefix: isProd ? "./" : "/",
   },
   server: {
     port: 7979,
